86206e7979f3acef3fc6c3fb369607ec65568539
NEO6667 <princeyashc7568@gmail.com>
2025-07-18 19:47:32 +0530

    added all extensions for tiptap

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps{
     params: Promise<{documentId: string }>;
 };
@@ -7,8 +9,9 @@ const DocumentIdPage = async({ params }: DocumentIdPageProps) => {
 
 
     return (
-        <div>
-            Document ID: {documentId}
+        <div className="min-h-screen bg-[#FAFBFD]">
+
+            <Editor />
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center bg-red-300 justify-center">
-      <p>hello world</p>
-      <Button>
-        click me
-      </Button>
+      Click &nbsp;<Link href="/documents/123">
+        <span className="text-blue-500 underline"> here </span></Link>&nbsp; to go to document id
     </div>
   );
 }
