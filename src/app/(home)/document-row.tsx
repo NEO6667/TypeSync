e184495e3d2da540f113a647a95a68a84e60a02e
NEO6667 <princeyashc7568@gmail.com>
2025-07-29 22:21:38 +0530

    search implement

--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -1,19 +1,27 @@
 import { SiGoogledocs } from "react-icons/si"
-import { Building2Icon, CircleUserIcon, MoreVertical } from "lucide-react";
+import { Building2Icon, CircleUserIcon } from "lucide-react";
 import { format } from "date-fns";
 
 import { TableCell, TableRow } from "@/components/ui/table";
 
 import { Doc } from "../../../convex/_generated/dataModel";
-import { Button } from "@/components/ui/button";
+import { DocumentMenu } from "./document-menu";
+import { useRouter } from "next/navigation";
 
 interface DocumentRowProps {
     document: Doc<"documents">;
 };
 
 export const DocumentRow = ({document} : DocumentRowProps) => {
+    const router = useRouter();
+
+    const onNewTabClick = (id: string) => {
+        window.open(`/documents/${id}`, "_blank")
+    }
+
     return (
         <TableRow
+            onClick={() => router.push(`/documents/${document._id}`)}
             className="cursor-pointer"
         >
             <TableCell className="w-[50px]" >
@@ -30,9 +38,11 @@ export const DocumentRow = ({document} : DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyyy")}
             </TableCell>
             <TableCell className="flex justify-end">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4" />
-                </Button>
+                <DocumentMenu 
+                    documentId = {document._id}
+                    title = {document.title}
+                    onNewTab = {onNewTabClick}
+                />
             </TableCell>
 
         </TableRow>
